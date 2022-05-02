a876f47fee6f07541c9e68c95a9051900b7c80c4
Kimchiman <asd14235714@gmail.com>
2022-05-02 18:30:07 +0900

    응용문제 최종2

--- app/src/main/java/com/neppplus/andman/MainActivity.kt
+++ app/src/main/java/com/neppplus/andman/MainActivity.kt
@@ -2,6 +2,7 @@ package com.neppplus.andman
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -9,10 +10,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         chagneBtn1.setOnClickListener {
             Toast.makeText(this, "회원 가입 불가", Toast.LENGTH_SHORT).show()
+            Log.d("MainActivity_회원가입","회원가입 시도됨")
+
         }
-        
+
         chagneBtn2.setOnClickListener {
             Toast.makeText(this, "로그인 성공", Toast.LENGTH_SHORT).show()
         }
